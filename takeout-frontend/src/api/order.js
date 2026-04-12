4f632ef01388e5a550c0971bd14b34feb668bf87
Maffile-Enky <2309832453@qq.com>
2026-04-12 21:47:27 +0800

    feat：前端登录界面

---
+++ takeout-frontend/src/api/order.js
@@ -0,0 +1,4 @@
+﻿import request from './request'
+
+export const createOrder = (data) => request.post('/order/create', data)
+export const getOrderList = (params) => request.get('/order/list', { params })
